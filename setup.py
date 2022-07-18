1483439b5ef69d1b9c44fdfe0c9b11127c70deb8
MoroccanTea <hamza.essad@ynov.com>
2022-07-18 02:39:38 +0100

    Initial project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pa55G3n',
+    version='1.0-alpha',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Hamza ESSAD',
+    author_email='hamza@essad.ma',
+    description='A simple password generator'
+)
